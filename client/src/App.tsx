e4979d6bf625ce5ed29fefc2245e4a51ffb7fdbc
Vishal <vbalagani8@gmail.com>
2023-12-06 22:05:10 -0500

    Updated with typescript, using antd now

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Button With Antd</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
