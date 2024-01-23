906e7d8f9f82a3c24b8c9b1fb8c4d5177cbe827b
Vishal <vbalagani8@gmail.com>
2024-01-23 12:06:49 -0500

    Course table page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,54 @@
 import React from "react";
-import logo from "./logo.svg";
+import CourseTable from "./Components/CourseTable.jsx";
 import "./App.css";
-import { Button } from "antd";
+import { Button, Alert, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button>Button With Antd</Button>
-      </header>
+      <div className="head">
+        <ul className="nav justify-content-center mams-red">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              Active
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <div className="card">
+          <div className="card-header">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">Test 1</li>
+                <li className="breadcrumb-item">Test 2</li>
+                <li className="breadcrumb-item active">Test 3</li>
+              </ol>
+            </nav>
+            <Alert variant="danger">This is an alert</Alert>
+            <Button>This is a bootstrap button</Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="card courses-card">
+        <h2 className="card-title">Available Courses</h2>
+        <CourseTable />
+      </div>
     </div>
   );
 }
---
+++ client/src/Components/CourseTable.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+
+
+function MyComponent() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+
+    let input = require("./Courses_v1.json");
+
+    let temp = [];
+
+    for(let i = 0; i < input.length; i++) {
+      let selectedData = {
+        CourseID: input[i].CID,
+        Title: input[i].TITLE,
+        Approver: input[i].Approver,
+        Terms: input[i].PTerms,
+        Prereq: input[i]["MAMS Prerequisites"],
+      };
+
+      temp.push(selectedData);
+
+    }
+
+    setData(temp);
+
+    console.log(temp)
+
+
+
+
+  }, []);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>CID</th>
+          <th>Title</th>
+          <th>Approver</th>
+          <th>Terms</th>
+          <th>Prerequisites</th>
+        </tr>
+      </thead>
+      <tbody>
+      {data.map((course, index) => (
+            <tr key={index}>
+              <td>{course.CourseID}</td>
+              <td>{course.Title}</td>
+              <td>{course.Approver}</td>
+              <td>{course.Terms}</td>
+              <td>{course.Prereq}</td>
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  );
+}
+
+export default MyComponent;
